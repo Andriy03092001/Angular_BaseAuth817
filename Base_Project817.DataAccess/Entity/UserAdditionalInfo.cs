2662de090cb76163ff2f75be850a53b2514fc08c
Andriy03092001 <cuanid236316@gmail.com>
2020-10-13 16:45:07 +0300

    first

---
+++ Base_Project817.DataAccess/Entity/UserAdditionalInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base_Project817.DataAccess.Entity
+{
+    public class UserAdditionalInfo
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
